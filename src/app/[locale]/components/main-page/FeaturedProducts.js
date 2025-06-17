587af53f1320dc8386d1a2c83c5b42ee6eb07524
seqenenra08 <juandavidalvarez08@gmail.com>
2025-06-16 22:40:15 -0500

    estructura de productos organizadas igual que los catalogos para ellos

--- src/app/[locale]/components/main-page/FeaturedProducts.js
+++ src/app/[locale]/components/main-page/FeaturedProducts.js
@@ -1,26 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "../general/card"; 
 import { useTranslations } from "use-intl";
-
-const products = [
-  { id: 1, name: "Message Basket Bag", price: "£75.00", image: "/chaqueta.jpg", isNew: true, imagepng: "/chaquetaSinFondo.png" },
-  { id: 2, name: "Printed Cotton Bag", price: "£160.00", image: "/chaqueta.jpg", isNew: true, imagepng: "/chaquetaSinFondo.png" },
-  { id: 3, name: "Pastel Bodycon Bag", price: "£55.00", image: "/chaqueta.jpg", isNew: true, imagepng: "/chaquetaSinFondo.png" },
-  { id: 4, name: "Victoria Secret Bags", price: "£65.00", image: "/chaqueta.jpg", isNew: false, imagepng: "/chaquetaSinFondo.png" },
-  { id: 5, name: "New Style Bag", price: "£78.00", image: "/chaqueta.jpg", isNew: true, imagepng: "/chaquetaSinFondo.png" },
-  { id: 6, name: "Viccent Bag", price: "£55.00", image: "/chaqueta.jpg", isNew: false, imagepng: "/chaquetaSinFondo.png" },
-  { id: 7, name: "Colorful Summer Bags", price: "£55.00", image: "/chaqueta.jpg", isNew: true, imagepng: "/chaquetaSinFondo.png" },
-  { id: 8, name: "Luxury Bag", price: "£90.00", image: "/chaqueta.jpg", isNew: false, imagepng: "/chaquetaSinFondo.png" },
-];
+import { products } from "@/app/[locale]/products.test";
 
 export function FeaturedProducts() {
   const [favorites, setFavorites] = useState({});
   const t = useTranslations("featuredProducts");
+  const [shuffled, setShuffled] = useState([]);
 
   const toggleFavorite = (id) => {
     setFavorites((prev) => ({ ...prev, [id]: !prev[id] }));
   };
+
+  useEffect(() => {
+    const copy = [...products];
+    const shuffledArray = copy.sort(() => Math.random() - 0.5).slice(0, 8);
+    setShuffled(shuffledArray);
+  }, [products]);
 
   return (
     <section className="py-10 text-white">
@@ -29,18 +26,24 @@ export function FeaturedProducts() {
 
       {/* Grid de productos */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <Card
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            cardImage={product.image}
-            isNew={product.isNew}
-            toggleFavorite={toggleFavorite}
-            favorites={favorites}
-            imagepng={product.imagepng}
-          />
+        {shuffled.map(product => (
+            <Card
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              cardImage={product.image}
+              isNew={product.isNew}
+              imagepng={product.imagepng}
+              type={product.type}
+              sale={product.sale}
+              description={product.description}
+              size={product.size}
+              color={product.color}
+              material={product.material}
+              favorites={favorites}
+              toggleFavorite={toggleFavorite}
+            />
         ))}
       </div>
     </section>
